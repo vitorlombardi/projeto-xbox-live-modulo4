26911b1ac40e0db14574ba437ec53c6b351912b2
vitorlombardi <83724421+vitorlombardi@users.noreply.github.com>
2021-09-14 15:43:47 -0300

    create: service usuario

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -3,34 +3,45 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class UsuarioService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return this.prisma.Usuarios.create({ data: createUsuarioDto });
-  }
+  async create(createUsuarioDto: CreateUsuarioDto) {
+    const data = {
+      ...createUsuarioDto,
+      senha: await bcrypt.hash(createUsuarioDto.senha, 10),
+    };
 
+    const createdUsuario = await this.prisma.usuarios.create({ data });
+
+    return {
+      ...createdUsuario,
+      senha: undefined,
+    };
+  }
   findAll() {
-    return this.prisma.Usuarios.findMany();
+    return this.prisma.usuarios.findMany();
   }
 
   findById(id: number) {
-    return this.prisma.Usuarios.findUnique({ where: { id } });
+    return this.prisma.usuarios.findUnique({ where: { id } });
   }
 
   findByEmail(email: string) {
-    return this.prisma.Usuarios.findUnique({ where: { email } });
+    return this.prisma.usuarios.findUnique({ where: { email } });
   }
 
   update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
-    return this.prisma.Usuarios.update({
+    return this.prisma.usuarios.update({
       where: { id },
       data: updateUsuarioDto,
     });
   }
 
   remove(id: number) {
-    return this.prisma.Usuarios.delete({ where: { id } });
+    return this.prisma.usuarios.delete({ where: { id } });
   }
 }
