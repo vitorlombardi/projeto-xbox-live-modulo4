086971ed679a6b51e997b88aa792820a052ea567
vitorlombardi <83724421+vitorlombardi@users.noreply.github.com>
2021-09-14 16:42:38 -0300

    create: interceptor

---
+++ src/interceptors/unauthorized.interceptor.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { UnauthorizedError } from 'src/erros/unauthorized.error';
+
+@Injectable()
+export class UnauthorizedInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError((error) => {
+        if (error instanceof UnauthorizedError) {
+          throw new UnauthorizedException(error.message);
+        } else {
+          throw error;
+        }
+      }),
+    );
+  }
+}
