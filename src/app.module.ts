9a161bca21ace5605cca38a43a98a55028d4dc10
vitorlombardi <83724421+vitorlombardi@users.noreply.github.com>
2021-09-14 14:34:26 -0300

    create: auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsuarioModule, PrismaModule],
+  imports: [UsuarioModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
